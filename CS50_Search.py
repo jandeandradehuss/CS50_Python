d352fae1e4f8a28a480dfab054b20e41df0da12c
Jan Huss <160907440+jandeandradehuss@users.noreply.github.com>
2026-03-01 22:00:13 +0100

    More methods to solve, but have identified potential thought errors

--- CS50_Search.py
+++ CS50_Search.py
@@ -15,25 +15,44 @@ class FrontierExplored:
     def appendNode(self, newNodes):
         if newNodes not in self.searchedNodes:
             self.toExploreNodes.append(newNodes)
+
+    def containsState(self, state):
+        return any(node.state == state for node in self.frontier)
+
+    def empty(self):
+        return len(self.frontier) == 0
     
     # Pop first element
     def removeNodeBreadth(self):
         node = self.toExploreNodes[0]
-        self.searchedNodes.append(self.toExploreNodes[0])
+        self.searchedNodes.append(node)
         self.toExploreNodes = self.toExploreNodes[1:]
         return node
 
     # Pop last element
     def removeNodeDepth(self):
         node = self.toExploreNodes[-1]
-        self.searchedNodes.append(self.toExploreNodes[-1])
+        self.searchedNodes.append(node)
         self.toExploreNodes = self.toExploreNodes[:-1]
         return node
 
 class Maze:
-    def __init__(self):
-        self.startState = None
-        self.goalState = None
+    def __init__(self,maze,start,goal):
+        self.maze = maze
+        self.startState = start
+        self.goalState = goal
+        self.width = None
+        self.height = None
+    
+    def getWidth(self):
+        if self.width == None:
+            self.width = len(self.maze)
+        return self.width
+
+    def getWidth(self):
+        if self.height == None:
+            self.height = len(self.maze)
+        return self.height
 
     def isStartState(self, el):
         return el == 'A'
@@ -46,14 +65,6 @@ class Maze:
 
     def isSpace(self, el):
         return el == ' '  # or '' if that is truly your design
-
-    def setStartState(self, el):
-        if self.isStartState(el):
-            self.startState = el
-
-    def setGoalState(self, el):
-        if self.isGoalState(el):
-            self.goalState = el
         
 
 class MazeSolver:
@@ -62,9 +73,33 @@ class MazeSolver:
 
     # def ations(self):
 
+    def neighbours(self,state):
+        print('TO DO')
+
     # def results(self):
 
-    # def solveMaze(self):
+    def solveMaze(self):
+
+        Start = Node(self.maze.startState, None, None, 1)
+        
+        Frontier = FrontierExplored()
+        Frontier.appendNode(Start)
+
+        while not Frontier.empty():
+            nextNode = Frontier.removeNodeBreadth()
+
+            if nextNode == self.maze.isGoalState():
+                Frontier.appendNode(nextNode)
+                break
+            else:
+                Frontier.appendNode(self.neighbours(nextNode))
+
+        if Frontier.searchedNodes[-1] == self.maze.isGoalState():
+            print('Solution Found!')
+            print(Frontier.searchedNodes)
+        else:
+            print('Error')
+        
 
 def Main():
 
